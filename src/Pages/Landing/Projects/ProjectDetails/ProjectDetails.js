a0cb603dc4d16cf5b1c6321cc6ed20d7d90183e5
ameen4455 <ameen4455@gmail.com>
2020-05-20 23:31:38 +0530

    added project tabs

---
+++ src/Pages/Landing/Projects/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./ProjectDetails.scss";
+import { Row, Col } from "react-bootstrap";
+
+const ProjectDetails = () => {
+  return (
+    <div className={"d-flex h-100 flex-column "}>
+      <Row>
+        <Col className={"p-5 shadow-bottom heading"}>
+          <div className={"flex-grow-1"}>
+            <div>
+              <h4>Name</h4>
+              <h5 className={"font-weight-light"}>Team Leader</h5>
+            </div>
+          </div>
+          <div className={" fix-flex left-right-margin"}>
+            <div>
+              <h4>Name</h4>
+              <h5 className={"font-weight-light"}>Team Leader</h5>
+            </div>
+          </div>
+          <div className={"fix-flex"}>
+            <div>
+              <h4>Name</h4>
+              <h5 className={"font-weight-light"}>Team Leader</h5>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row className={"p-5 flex-grow-1 overflow"}>
+        <div>
+          <h5>Heading</h5>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+          <div>
+              <h5>Heading</h5>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+          <div>
+              <h5>Heading</h5>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+          <div>
+              <h5>Heading</h5>
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+      </Row>
+    </div>
+  );
+};
+
+export default ProjectDetails;
