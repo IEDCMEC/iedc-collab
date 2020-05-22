e36c08a1da1014dc7ab9618dcab434324be9e45d
MeenakshiShankar <me30shankar@gmail.com>
2020-05-22 07:45:04 +0530

    bookmark added

--- src/Pages/Landing/Projects/ProjectList/ProjectList.js
+++ src/Pages/Landing/Projects/ProjectList/ProjectList.js
@@ -6,12 +6,7 @@ const ProjectList = () =>{
     return(
         <Col className={" overflow"}>
         
-        <div className="content post-item">
-        <svg class="bi bi-bookmark-fill" width="2em" height="2em" viewBox="0 0 16 16" fill="black" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M3 3a2 2 0 012-2h6a2 2 0 012 2v12l-5-3-5 3V3z" clip-rule="evenodd"/>
-</svg>
-
-            <ProjectBox name={"Name"} skills={["a","b"]} projectName={"Ca"}></ProjectBox></div>
+        <div className="content post-item"><ProjectBox name={"Name"} skills={["a","b"]} projectName={"Ca"}></ProjectBox></div>
         <div className="content post-item"><ProjectBox name={"Name"} skills={["a","b"]} projectName={"Ca"} /></div>
         <div className="content post-item"><ProjectBox name={"Name"} skills={["a","b"]} projectName={"Ca"} /></div>
         <div className="content post-item"><ProjectBox name={"Name"} skills={["a","b"]} projectName={"Ca"} /></div>
