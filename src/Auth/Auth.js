34ea468347e10399bee554bd0cc1d5ffd22e6ea6
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-12-10 17:32:29 +0530

    added google sign in on login page

---
+++ src/Auth/Auth.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+//import app from "./base.js";
+import firebase from "firebase";
+import validate from './Validate'
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setPending(false)
+    });
+  }, []);
+
+  if(pending){
+    return <>Loading...</>
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./Landing.css";
 import { useHistory } from 'react-router-dom';
 
+
+//import {signIn} from '../Firebase/auth';
+
 const Landing = (props) => {
   const history = useHistory();
   // const signUp = () => {
@@ -12,12 +15,15 @@ const Landing = (props) => {
     <div>
       <div class="landing">
         <div className="collab">
+          
           <div className="centered">
+             
             <button class="btn" onClick={() => { history.push("/collab"); }}>
                 <b>Collab</b>
             </button> <br /> <br />
               <b>!What is collab?</b>
-              {/* <button class="btn" onClick={() => { signUp(); }}k>LoginWithGoogle</button> */}
+             
+              { /*<button class="btn" onClick={() => { signIn(); }}k>LoginWithGoogle</button>*/ }
           </div>
         </div>
         <div className="work_at_mec">
@@ -34,3 +40,65 @@ const Landing = (props) => {
 }
 
 export default Landing;
+/*--------------------------------------------
+
+import { useEffect, useState, useRef } from "react";
+import "./Styles/Landing.css";
+import { useHistory } from "react-router-dom";
+import img from "../assets/robovitics.png";
+import google from "../assets/signin.svg";
+import svg from "../assets/landingPhoto.svg";
+import svg2 from "../assets/landingPhoto2.svg";
+import help from "../assets/Help.svg";
+import firebase from "firebase";
+import { signIn } from "../firebase-codes";
+import { Redir } from "../HandleRedir";
+
+const Landing = ({ updateMail }) => {
+	const history = useHistory();
+	const [width, setWidth] = useState(window.innerWidth);
+	const [auth, updateAuth] = useState(false);
+	const [helpp, updateHelp] = useState(false);
+	// const [mail, updateMail] = useState("");
+
+	const logo = useRef(svg);
+	const handleResize = () => setWidth(window.innerWidth);
+	if (width < 801) {
+		logo.current = svg2;
+	} else logo.current = svg;
+
+	useEffect(() => {
+		firebase.auth().signOut();
+
+		const uns = firebase.auth().onAuthStateChanged((user) => {
+			updateAuth(!!user);
+			// console.log(user, "asd");
+
+			if (user) {
+				updateMail(user.email);
+
+				if (history.location.pathname == "/") {
+					if (Redir(user.email)) {
+						history.push("/register");
+						// console
+					} else {
+						// firebase.auth().signOut();
+						history.push("/error");
+					}
+				}
+			}
+		});
+		const handleResize = () => setWidth(window.innerWidth);
+		window.addEventListener("resize", handleResize);
+		return () => {
+			uns();
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+	return (
+
+	);
+};
+
+export default Landing;
+*/
