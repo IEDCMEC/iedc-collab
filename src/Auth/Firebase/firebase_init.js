e5472836f118ef4e603308d88a9958f0dc6d35ea
Vivek R <123vivekr@gmail.com>
2020-12-11 17:37:54 +0530

    add firebase database API

--- src/Auth/Firebase/firebase_init.js
+++ src/Auth/Firebase/firebase_init.js
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -20,6 +21,7 @@ export const initialize = () => {
 };
 
 	
+// Authentication functions
 
 export const signIn = async () => {
 	const provider = new firebase.auth.GoogleAuthProvider();
@@ -43,6 +45,7 @@ export const signIn = async () => {
 	}
 };
 
+
 export const signOut = () => {
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
@@ -51,4 +54,91 @@ export const signOut = () => {
 		console.log("Sign out unsuccessful");
 		alert('Something is wrong, please check network connection')
       });
-  } 
+};
+
+
+// Firebase Realtime Database functions
+
+export const doCreateProject = (name, desc, links) => {
+	let user = firebase.auth().currentUser;
+	if(!user) {
+		alert('Please login to add a project');
+		return;
+	}
+
+	let uid = user.uid;
+    const updatedAt = Date.now();
+	var newProjectID = firebase.database().ref().child("projects").push().key;
+    var projectData = {
+      name: name,
+      available: "true",
+      desc: desc,
+      links: links,
+      updatedAt: updatedAt,
+      leader_id: uid,
+	};
+
+	firebase.database()
+		.ref("projects/" + newProjectID)
+		.set(projectData)
+		.then(function() {
+			console.log("Project added sucessfully");
+		}).catch(function(error) {
+			alert('Something went wrong');
+			console.log(error);
+		});
+};
+
+
+export const doDeleteProject = (project_id) => {
+	let user = firebase.auth().currentUser;
+	if(!user) {
+		alert('Please login to add a project');
+		return;
+	}
+
+	let projectRef = firebase.database()
+						.ref("projects/" + project_id);
+
+	projectRef.child("leader_id").once("value").then(function(snapshot) {
+		if(snapshot.val() != user.uid) {
+			return;
+		}
+	});
+
+	projectRef
+		.remove()
+		.then(function() {
+			console.log("Project deleted sucessfully");
+		}).catch(function(error) {
+			alert('Something went wrong');
+			console.log(error);
+		});
+};
+
+
+export const getProjects = () => {
+	firebase.database()
+		.ref("projects/")
+		.once("value")
+		.then(function(snapshot) {
+			console.log(snapshot.val());
+		}).catch(function(error) {
+			alert('Something went wrong');
+			console.log(error);
+		});
+}
+
+
+export const getProject = (project_id) => {
+	firebase.database()
+	.ref("projects/")
+	.child(project_id)
+	.once("value")
+	.then(function(snapshot) {
+		console.log(snapshot.val());
+	}).catch(function(error) {
+		alert('Something went wrong');
+		console.log(error);
+	});
+}
