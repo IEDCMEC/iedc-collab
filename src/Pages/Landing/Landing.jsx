eed8d2854d8210a67719ff6cd6aa78906b03e2b5
ameen4455 <ameen4455@gmail.com>
2020-12-14 20:26:54 +0530

    styled landing page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,36 +1,69 @@
 import React from "react";
-import "./Landing.css";
-import { useHistory } from 'react-router-dom';
+import "./Landing.scss";
+import { useHistory } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Collab from "../../assets/collab.png";
+import Work from "../../assets/work.png";
+import Logo from "../../assets/logo.svg";
 
 const Landing = (props) => {
-  const history = useHistory();
+  // const history = useHistory();
   // const signUp = () => {
   //   console.log("siged up");
   //   props.firebase.doSignInWithGoogle();
   // };
+
+  // {/* <button class="btn" onClick={() => { signUp(); }}k>LoginWithGoogle</button> */}
+
   return (
-    <div>
-      <div class="landing">
-        <div className="collab">
-          <div className="centered">
-            <button class="btn" onClick={() => { history.push("/collab"); }}>
-                <b>Collab</b>
-            </button> <br /> <br />
-              <b>!What is collab?</b>
-              {/* <button class="btn" onClick={() => { signUp(); }}k>LoginWithGoogle</button> */}
-          </div>
-        </div>
-        <div className="work_at_mec">
-          <div className="centered">
-            <button class="btn" onClick={() => { history.push("/workatmec"); }}>
-                <b>Work At MEC</b>
-            </button> <br /> <br />
-            <b>!What is work at mec?</b>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Container className={"landing"}>
+        <Row>
+          <Col>
+            <img src={Logo} className={"mt-5"} alt="" />
+          </Col>
+        </Row>
+        <Col className={"ml-auto mr-auto text-center text-md-left"} md={8}>
+          <Row
+            className={
+              "box mt-5 p-3 pt-4 pb-4 justify-content center flex-column-reverse flex-md-row align-items-md-center"
+            }
+          >
+            <Col className={"mt-3 mt-md-0"} md={8}>
+              <h4>IEDC COLLAB PLATFORM</h4>
+              <p>
+                What is collab and lorem ipsum des dolor set emet.What is collab
+                and lorem ipsum des dolor set emet.What is collab and lorem
+                ipsum des dolor set emet.What is collab and lorem ipsum des
+                dolor set emet.
+              </p>
+            </Col>
+            <Col className={"d-flex justify-content-center align-items-center"}>
+              <img src={Collab} alt="" />
+            </Col>
+          </Row>
+          <Row
+            className={
+              "box mt-5 p-3 pt-4 mb-4 pb-4 justify-content center flex-column-reverse flex-md-row align-items-md-center"
+            }
+          >
+            <Col className={"mt-3 mt-md-0"} md={8}>
+              <h4>WORK AT MEC</h4>
+              <p>
+                What is collab and lorem ipsum des dolor set emet.What is collab
+                and lorem ipsum des dolor set emet.What is collab and lorem
+                ipsum des dolor set emet.What is collab and lorem ipsum des
+                dolor set emet.
+              </p>
+            </Col>
+            <Col className={"d-flex justify-content-center align-items-center"}>
+              <img src={Work} alt="" />
+            </Col>
+          </Row>
+        </Col>
+      </Container>
+    </>
   );
-}
+};
 
 export default Landing;
