4abaf2222ad19f048a245a23dc2b2263d1883654
Vishruth S <vs.vishruth.s@gmail.com>
2020-12-27 13:12:49 +0530

    :sparkles: Added form page

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import Landing from "./Pages/Landing/Landing";
 import Collab from "./Pages/Collab/Collab";
 import WorkAtMEC from "./Pages/WorkAtMEC/WorkAtMEC";
 import Login from './Pages/Login/Login';
-import {initialize} from './Firebase/firebase';
+import Form from './Pages/Form/Form'
+import { initialize } from './Firebase/firebase';
 import PrivateRoute from './Firebase/Auth/PrivateRoute';
 initialize()
 function App() {
   return (
-      <AuthProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/login" component={Login}/>
-            <PrivateRoute exact path="/" component={Landing} />
-            <PrivateRoute Route exact path="/collab" component={Collab} />
-            <PrivateRoute Route exact path="/workatmec" component={WorkAtMEC} />
-          </Switch>
-        </BrowserRouter>
-      </AuthProvider>
- 
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/form" component={Form} />
+          <PrivateRoute exact path="/" component={Landing} />
+          <PrivateRoute Route exact path="/collab" component={Collab} />
+          <PrivateRoute Route exact path="/workatmec" component={WorkAtMEC} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
+
   );
 }
 export default App;
