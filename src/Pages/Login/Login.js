72f49acf42a5203cd28df23d8938ba151ccce831
joeljosh <jjpadippura123@gmail.com>
2020-12-28 17:13:21 +0530

    Login Page Updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,9 +2,13 @@ import React, { useContext,useState } from "react";
 import { signIn,signOut } from '../../Firebase/firebase';
 import GoogleButton from 'react-google-button';
 import { AuthContext,validUserContext } from "../../Firebase/Auth/Auth";
+import { Container, Row, Col } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import {withRouter, Redirect} from 'react-router-dom'
 import logIn from './googleLogin'
+import logo from '../../assets/logo.png'
+
+import './Login.css'
 /*
 const logIn= () =>{
     return(
@@ -61,8 +65,7 @@ const GoogleLogIn = () =>{
         signOut();
          return (
 
-    <div>
-     <h3>Please Sign In </h3>
+    <div className="signInButton">
     <GoogleButton  
         label = "sign in"
         onClick={
@@ -97,4 +100,33 @@ const GoogleLogIn = () =>{
     )
 }
 
-export default withRouter(GoogleLogIn);
+const login = () => {
+    return(
+        <div className="main">
+            
+            <div className="login-container">
+
+                <Row className={"d-flex justify-content-center align-items-center"}>
+                    <img src={logo} className={"mr-2"} alt="" />
+                </Row>
+                <Row className={"d-flex justify-content-center align-items-center"}>
+                    <p className="intro">
+                        Welcome to Collab, a Place exclusively for MECians to find like minded people with desired skillsets to Collaborate
+                        on your project.
+                        <br></br>
+                        Work At Mec also provides internship opportunities listed by the Placement Cell
+                    </p>
+                    <p className="intro2">
+                        Sign in to Continue
+                    </p>
+                </Row>
+                <Row className={"d-flex justify-content-center align-items-center"}>
+                    <GoogleLogIn />
+                </Row>
+
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(login);
