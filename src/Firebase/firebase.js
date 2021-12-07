61f77b424c0bd42715eefbec92940e99dd05dc3a
Aldrin Jenson <aldrinjenson@gmail.com>
2021-12-07 23:48:09 +0530

    Chang API_KEY name

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: process.env.REACT_APP_FB_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
