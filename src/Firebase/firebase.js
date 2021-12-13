91782b92b43ff99075be8173dd085e7e5b1fc12e
Aldrin Jenson <aldrinjenson@gmail.com>
2021-12-13 18:38:53 +0530

    Prettier + Refactoring

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,202 +3,190 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
-
 
 export const initialize = () => {
-	if (!firebase.apps.length) {
-		 firebase.initializeApp(config);
-	} else {
-		firebase.app(); // if already initialized, use that one
-	}
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
 };
 
-	
 // Authentication functions
-
 export const signIn = async () => {
-	const provider = new firebase.auth.GoogleAuthProvider();
-	provider.setCustomParameters({
-		prompt: "select_account",
-	});
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: "select_account",
+  });
 
-	// firebase.auth().signOut();
-	try {
-		const result = await firebase.auth().signInWithPopup(provider);
-		var user = result.user;
-		console.log(user);
+  // firebase.auth().signOut();
+  try {
+    const result = await firebase.auth().signInWithPopup(provider);
+    var user = result.user;
+    console.log(user);
 
-		const userData = {
-			name: user.displayName,
-			email: user.email,
-		};
-		firebase.database()
-		.ref("users/" + user.uid)
-		.set(userData)
-		.then(function() {
-			console.log("User added sucessfully");
-		}).catch(function(error) {
-			alert('Something went wrong');
-			console.log(error);
-		});
+    const userData = {
+      name: user.displayName,
+      email: user.email,
+    };
+    firebase
+      .database()
+      .ref("users/" + user.uid)
+      .set(userData)
+      .then(function () {
+        console.log("User added sucessfully");
+      })
+      .catch(function (error) {
+        alert("Something went wrong");
+        console.log(error);
+      });
 
-		return true;
-	} catch (error) {
-		alert('Something is wrong, please check network connection')
-		console.log(error);
-		// var errorCode = error.code;
-		// var errorMessage = error.message;
-		// var email = error.email;
-		// var credential = error.credential;
-	}
+    return true;
+  } catch (error) {
+    alert("Something is wrong, please check network connection");
+    console.log(error);
+  }
 };
-
 
 export const signOut = () => {
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-        console.log("Sign out successful");
-      }).catch(function(error) {
-		console.log("Sign out unsuccessful");
-		alert('Something is wrong, please check network connection')
-      });
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      // Sign-out successful.
+      console.log("Sign out successful");
+    })
+    .catch(function (error) {
+      console.log("Sign out unsuccessful");
+      alert("Something is wrong, please check network connection");
+    });
 };
-
 
 // Firebase Realtime Database functions
 
 export const doCreateProject = (name, desc, links) => {
-	let user = firebase.auth().currentUser;
-	if(!user) {
-		alert('Please login to add a project');
-		return;
-	}
+  let user = firebase.auth().currentUser;
+  if (!user) {
+    alert("Please login to add a project");
+    return;
+  }
 
-	let uid = user.uid;
-	let leaderName = user.displayName;
-    const updatedAt = Date.now();
-	var newProjectID = firebase.database().ref().child("projects").push().key;
-    var projectData = {
-      name: name,
-      available: "true",
-      desc: desc,
-      links: links,
-      updatedAt: updatedAt,
-	  leader_id: uid,
-	  leader_name: leaderName,
-	};
+  let uid = user.uid;
+  let leaderName = user.displayName;
+  const updatedAt = Date.now();
+  var newProjectID = firebase.database().ref().child("projects").push().key;
+  var projectData = {
+    name: name,
+    available: "true",
+    desc: desc,
+    links: links,
+    updatedAt: updatedAt,
+    leader_id: uid,
+    leader_name: leaderName,
+  };
 
-	firebase.database()
-		.ref("projects/" + newProjectID)
-		.set(projectData)
-		.then(function() {
-			console.log("Project added sucessfully");
-		}).catch(function(error) {
-			alert('Something went wrong');
-			console.log(error);
-		});
+  firebase
+    .database()
+    .ref("projects/" + newProjectID)
+    .set(projectData)
+    .then(function () {
+      console.log("Project added sucessfully");
+    })
+    .catch(function (error) {
+      alert("Something went wrong");
+      console.log(error);
+    });
 };
-
 
 export const doDeleteProject = (project_id) => {
-	let user = firebase.auth().currentUser;
-	if(!user) {
-		alert('Please login to add a project');
-		return;
-	}
+  let user = firebase.auth().currentUser;
+  if (!user) {
+    alert("Please login to add a project");
+    return;
+  }
 
-	let projectRef = firebase.database()
-						.ref("projects/" + project_id);
+  let projectRef = firebase.database().ref("projects/" + project_id);
 
-	projectRef.child("leader_id").once("value").then(function(snapshot) {
-		if(snapshot.val() !== user.uid) {
-			return;
-		}
-	});
+  projectRef
+    .child("leader_id")
+    .once("value")
+    .then(function (snapshot) {
+      if (snapshot.val() !== user.uid) {
+        return;
+      }
+    });
 
-	projectRef
-		.remove()
-		.then(function() {
-			console.log("Project deleted sucessfully");
-		  })
-		  .catch(function(error) {
-			alert('Something went wrong');
-			console.log(error);
-		  })
+  projectRef
+    .remove()
+    .then(function () {
+      console.log("Project deleted sucessfully");
+    })
+    .catch(function (error) {
+      alert("Something went wrong");
+      console.log(error);
+    });
 };
 
-
 export const getProjects = () => {
-	return firebase.database()
-		.ref("projects/")
-		.once("value")		
-}
-
+  return firebase.database().ref("projects/").once("value");
+};
 
 export const getProject = (project_id) => {
-	return firebase.database()
-	.ref("projects/")
-	.child(project_id)
-	.once("value")
-}
+  return firebase.database().ref("projects/").child(project_id).once("value");
+};
 
 export const getUser = (user_id) => {
-	return firebase.database()
-	.ref("users/")
-	.child(user_id)
-	.once("value")
-}
+  return firebase.database().ref("users/").child(user_id).once("value");
+};
 
 export const getCompany = (company_id) => {
-	return firebase.database()
-	.ref("companies/")
-	.child(company_id)
-	.once("value")
-}
+  return firebase.database().ref("companies/").child(company_id).once("value");
+};
 
 export const getInternships = () => {
-	return firebase.database()
-		.ref("internships/")
-		.once("value")		
-}
+  return firebase.database().ref("internships/").once("value");
+};
 
 export const getInternship = (internship_id) => {
-	return firebase.database()
-	.ref("internships/")
-	.child(internship_id)
-	.once("value")
-}
+  return firebase
+    .database()
+    .ref("internships/")
+    .child(internship_id)
+    .once("value");
+};
 
 export const doDeleteInternship = (internship_id) => {
-	let user = firebase.auth().currentUser;
-	if(!user) {
-		alert('Please login to add a project');
-		return;
-	}
+  let user = firebase.auth().currentUser;
+  if (!user) {
+    alert("Please login to add a project");
+    return;
+  }
 
-	let internshipRef = firebase.database()
-						.ref("internships/" + internship_id);
+  let internshipRef = firebase.database().ref("internships/" + internship_id);
 
-	internshipRef.child("leader_id").once("value").then(function(snapshot) {
-		if(snapshot.val() !== user.uid) {
-			return;
-		}
-	});
+  internshipRef
+    .child("leader_id")
+    .once("value")
+    .then(function (snapshot) {
+      if (snapshot.val() !== user.uid) {
+        return;
+      }
+    });
 
-	internshipRef
-		.remove()
-		.then(function() {
-			console.log("internship deleted sucessfully");
-		  })
-		  .catch(function(error) {
-			alert('Something went wrong');
-			console.log(error);
-		  })
+  internshipRef
+    .remove()
+    .then(function () {
+      console.log("internship deleted sucessfully");
+    })
+    .catch(function (error) {
+      alert("Something went wrong");
+      console.log(error);
+    });
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,124 +1,56 @@
 import React, { useContext } from "react";
-import { signIn,signOut } from '../../Firebase/firebase';
-import GoogleButton from 'react-google-button';
-import { AuthContext,validUserContext } from "../../Firebase/Auth/Auth";
-import { Row} from "react-bootstrap";
-import {withRouter, Redirect} from 'react-router-dom'
-import logo from '../../assets/logo.png'
+import { signIn, signOut } from "../../Firebase/firebase";
+import GoogleButton from "react-google-button";
+import { AuthContext, validUserContext } from "../../Firebase/Auth/Auth";
+import { Row } from "react-bootstrap";
+import { withRouter, Redirect } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Login.css";
 
-import './Login.css'
-/*
-const logIn= () =>{
-    return(
-        <div style={{justifyContent:"center",margin:"0 Auto"}}>
-    <GoogleButton  
-        label = "sign in"
-        onClick={
-            () => { 
-                signIn();
-                console.log('Google button clicked') 
-            }
-        }
-    />
-    </div>
-    )
-}*/
-
-const GoogleLogIn = () =>{
-    const {currentUser} = useContext(AuthContext);
-    const {validUserState} = useContext(validUserContext)
-    //const [validUserState, updateValidity] = useState(false)
-    /*
-    const logIn = () =>{
-    signIn();
-    }*/
-    //&& validUserState
-    //&& currentUser.email.includes("vitstudent.ac.in")
-    // if(!!currentUser ){
-    //     if(currentUser.email.includes("mec.ac.in")){
-    //         console.log("in redirect" + validUserState)
-    //         return  <Redirect to="/" />
-    //     }
-    //     else{
-    //         return (
-
-    // <div>
-    //  <h3>Please Sign In with @mec.ac.in </h3>
-    // <GoogleButton  
-    //     label = "sign in"
-    //     onClick={
-    //         () => { 
-    //             signIn();
-    //             console.log('Google button clicked') 
-    //         }
-    //     }
-    // />
-    // </div>
-    
-    // )
-    //     }
-        
-    // }
-    if(!!currentUser ){
-     
-        console.log("in redirect" + validUserState)
-        return  <Redirect to="/" />
-  
-
- 
-        
-    }
-    else{
-        signOut();
-         return (
-
+const GoogleLogIn = () => {
+  const { currentUser } = useContext(AuthContext);
+  const { validUserState } = useContext(validUserContext);
+  if (currentUser) {
+    console.log("in redirect" + validUserState);
+    return <Redirect to="/" />;
+  }
+  // signOut();  //  why is this needed here ??
+  return (
     <div className="signInButton">
-    <GoogleButton  
-        label = "sign in"
-        onClick={
-            () => { 
-                signIn();
-                console.log('Google button clicked') 
-            }
-        }
-    />
+      <GoogleButton
+        label="sign in"
+        onClick={() => {
+          signIn();
+          console.log("Google button clicked");
+        }}
+      />
     </div>
-    
-    )
-        
-        
-       // return  <Redirect to="/error" />
-    }
-
-}
+  );
+};
 
 const login = () => {
-    return(
-        <div className="main">
-            
-            <div className="login-container">
-
-                <Row className={"d-flex justify-content-center align-items-center"}>
-                    <img src={logo} className={"mr-2"} alt="" />
-                </Row>
-                <Row className={"d-flex justify-content-center align-items-center"}>
-                    <p className="intro">
-                        Welcome to Collab, a Place exclusively for MECians to find like minded people with desired skillsets to Collaborate
-                        on your project.
-                        <br></br>
-                        Work At Mec also provides internship opportunities listed by the Placement Cell
-                    </p>
-                    <p className="intro2">
-                        Sign in to Continue
-                    </p>
-                </Row>
-                <Row className={"d-flex justify-content-center align-items-center"}>
-                    <GoogleLogIn />
-                </Row>
-
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="main">
+      <div className="login-container">
+        <Row className={"d-flex justify-content-center align-items-center"}>
+          <img src={logo} className={"mr-2"} alt="" />
+        </Row>
+        <Row className={"d-flex justify-content-center align-items-center"}>
+          <p className="intro">
+            Welcome to Collab, a Place exclusively for MECians to find like
+            minded people with desired skillsets to Collaborate on your project.
+            <br></br>
+            Work At Mec also provides internship opportunities listed by the
+            Placement Cell
+          </p>
+          <p className="intro2">Sign in to Continue</p>
+        </Row>
+        <Row className={"d-flex justify-content-center align-items-center"}>
+          <GoogleLogIn />
+        </Row>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(login);
