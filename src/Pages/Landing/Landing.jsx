aa68e70a58c39925c4b9dc1ef9fab9b5bce55e88
Sona Mathew <sonamathewc@gmail.com>
2021-12-14 21:55:25 +0530

    Feat:Collab home page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -13,16 +13,38 @@ import { ProjectContext } from "../../contexts/ProjectContext";
 const Landing = (props) => {
   const { projects } = useContext(ProjectContext);
   // console.log(projects)2
+
   return (
     <>
       <Container className={"landing"}>
-        <h3 style={{ textAlign: "center" }}>Projects</h3>
+        <h3 style={{ textAlign: "center" }}>PROJECTS</h3>
 
-        {/* {projects.map((project) => (
-          <div>
-            <h2>{project.name}</h2>
+        {projects.map((project) => (
+          <div className="cards">
+            <div className="card">
+              <div className="card__image-holder">
+                <img
+                  className="card__image"
+                  src="https://images.unsplash.com/photo-1639413665566-2f75adf7b7ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                  alt="wave"
+                />
+              </div>
+              <div className="card-title">
+                <h2 className="justify-content-center">
+                  {project.name}
+                  <small className="justify-content-center">
+                    {" "}
+                    {project.leader_name}
+                  </small>
+                </h2>
+              </div>
+            </div>
           </div>
-        ))} */}
+
+          // <div>
+          //   <h2>{project.name}</h2>
+          // </div>
+        ))}
       </Container>
     </>
   );
