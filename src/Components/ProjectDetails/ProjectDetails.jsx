d530b7f66539c640764a5153b088185c6e530cbb
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-12-28 12:00:13 +0530

    centered delete confirmation modal

--- src/Components/ProjectDetails/ProjectDetails.jsx
+++ src/Components/ProjectDetails/ProjectDetails.jsx
@@ -13,26 +13,10 @@ import Link from "../../assets/Link.png";
 import Navigate from "../../assets/Navigate.png";
 import ProjectModal from "../ProjectModal/ProjectModal";
 import { Modal, Button } from "react-bootstrap";
+import DeleteConfirmation from "../DeleteConfirmationModal/DeleteConfirmation";
 
 
-const DeleteConfirmation = ({ showModal, hideModal, confirmModal, id}) => {
-  return (
-      <Modal show={showModal} onHide={hideModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete Confirmation</Modal.Title>
-      </Modal.Header>
-      <Modal.Body><div className="alert alert-danger">Are you sure you want to delete this Project?</div></Modal.Body>
-      <Modal.Footer>
-        <Button variant="default" onClick={hideModal}>
-          Cancel
-        </Button>
-        <Button variant="danger" onClick={() => {confirmModal( id)} }>
-          Delete
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  )
-}
+
 
 const ProjectDetails = (props) => {
   const { selectedProject } = useContext(ProjectContext);
