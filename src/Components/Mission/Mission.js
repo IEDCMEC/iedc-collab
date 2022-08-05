dffc82faab767cc791b44d9c367d567f7cd41c40
jaison080 <jaisondennis080@gmail.com>
2022-08-05 18:54:00 +0530

    Made some components of Landing Page

---
+++ src/Components/Mission/Mission.js
@@ -0,0 +1,21 @@
+import "./Mission.scss";
+import bubble2 from '../../assets/bubble_2.svg';
+const Mission = () => {
+  return (
+    <div className="mission_main">
+      <div className="heading_mission">OUR MISSION</div>
+      <div className="paragraph_mission">
+        IEDC-MEC shall promote and foster entrepreneurial culture in the college
+        and will provide a platform for the students to pursue entrepreneurial
+        activities.
+        <br />
+        <br />
+        It shall provide assistance to potential entrepreneurs of MEC, conduct
+        activities that develop entrepreneurial qualities in students and shall
+        provide common facilities to students working on start-ups.
+      </div>
+      <img src={bubble2} alt="" className="bubble_2" />
+    </div>
+  );
+};
+export default Mission;
