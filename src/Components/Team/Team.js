236e8d49700bed741474db502f451dfec8feaf66
jaison080 <jaisondennis080@gmail.com>
2022-08-06 19:05:21 +0530

    Refactoring Complete

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,5 +1,5 @@
 import "./Team.scss";
-import bubble4 from '../../assets/bubble_4.svg';
+
 import bubble5 from '../../assets/bubble_5.svg';
 import TeamCard from "./TeamCard";
 const Team = () => {
@@ -10,7 +10,7 @@ const Team = () => {
   <div className="team">
     <TeamCard/>
   </div>
-  <img src={bubble4} alt="" className="bubble_4" />
+ 
   <img src={bubble5} alt="" className="bubble_5" />
 </div>
   );
