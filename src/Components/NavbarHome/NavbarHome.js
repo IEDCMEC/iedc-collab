f16102ea2bb77b60ebc0aea0280be811c58f752a
jaison080 <jaisondennis080@gmail.com>
2022-08-16 14:29:54 +0530

    made landing page minor changes and navbar

---
+++ src/Components/NavbarHome/NavbarHome.js
@@ -0,0 +1,109 @@
+import { useState, React } from "react";
+import { Drawer } from "@mui/material";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import home from "../../assets/home.svg";
+import "./NavbarHome.scss";
+import meclogo from "../../assets/meclogo.png";
+
+function NavbarHome() {
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="navbar_1">
+      <div className="nav__desktop">
+        <Link to="/" className="nav_item">
+          <div className="logo">
+            <div className="home_logo">
+              <img src={home} alt="" className="home__logo" />
+            </div>
+            <div className="title">IEDC MEC COLLAB</div>
+          </div>
+        </Link>
+        <div className="navbar_items">
+          <Link to="/" className="nav_item">
+            Ideas
+          </Link>
+          <Link to="/projects" className="nav_item">
+            Projects
+          </Link>
+          <Link to="/" className="nav_item">
+            Developers
+          </Link>
+        </div>
+        <a href="https://www.mec.ac.in/" target="_blank" rel="noreferrer">
+          <img src={meclogo} alt="" className="meclogo" />
+        </a>
+      </div>
+
+      <div className="nav__mob">
+        <div className="nav__mob_container">
+          <div className="title">IEDC MEC COLLAB</div>
+          <button className="nav_btn" onClick={handleDrawerOpen}>
+            <HiOutlineMenuAlt3 className="hamburger" />
+          </button>
+        </div>
+      </div>
+      <Drawer
+        open={open}
+        onClick={handleDrawerClose}
+        onClose={(event, reason) => {
+          if (reason !== "backdropClick") {
+            handleDrawerClose();
+          } else if (reason !== "escapeKeyDown") {
+            handleDrawerClose();
+          }
+        }}
+        anchor="left"
+      >
+        <div className="nav__drawer">
+          <div className="nav__drawer_header">
+            <div
+              className="title_mob"
+              data-aos="fade-right"
+              data-aos-duration="600"
+            >
+              IEDC MEC COLLAB
+            </div>
+            <div className="navbar_items_mob"  data-aos="fade-right"
+              data-aos-duration="600">
+              <Link to="/" className="nav_item_mob" onClick={handleDrawerClose}>
+                Ideas
+              </Link>
+              <Link
+                to="/projects"
+                className="nav_item_mob"
+                spy={true}
+                smooth={true}
+                onClick={handleDrawerClose}
+              >
+                Projects
+              </Link>
+              <Link
+                to="/"
+                className="nav_item_mob"
+                spy={true}
+                smooth={true}
+                onClick={handleDrawerClose}
+              >
+                Developers
+              </Link>
+            </div>
+          </div>
+          <a href="https://www.mec.ac.in/" target="_blank" rel="noreferrer" >
+            <img src={meclogo} alt="" className="meclogo__mob" />
+          </a>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+
+export default NavbarHome;
