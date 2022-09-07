0b854c53a6f27227de41d83698e7e281d69b0154
jaison080 <jaisondennis080@gmail.com>
2022-09-07 14:52:35 +0530

    Made Project Toggle Functional

--- src/Components/ProjectToggle/ProjectToggle.js
+++ src/Components/ProjectToggle/ProjectToggle.js
@@ -7,13 +7,13 @@ const ProjectToggle = () => {
         <div className='project-toggle__container'>
 
             <div className='project-toggle__title'> PROJECT LIFEBOAT</div>
-
-            <div className='project-toggle__options'>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/description'}>Description</NavLink>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/requirements'}>Requirements</NavLink>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/discussion'}>Discussion</NavLink>
-
+            <div className="project-toggle__NavbarItems">
+                                <NavLink exact activeClassName="project-toggle__NavLinksActive" className="project-toggle__NavLinks__desc" to='/description'>Description</NavLink>
+                                <NavLink exact activeClassName="project-toggle__NavLinksActive" className="project-toggle__NavLinks" to='/requirements'>Requirements</NavLink>
+                                <NavLink exact activeClassName="project-toggle__NavLinksActive" className="project-toggle__NavLinks__disc" to='/discussion'>Discussion</NavLink>
+                    
             </div>
+           
 
         </div>
 
