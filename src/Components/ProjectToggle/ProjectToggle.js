8437235c38a6c5e6aef8d2f452f493f76540326f
jaison080 <jaisondennis080@gmail.com>
2022-09-07 15:21:59 +0530

    Minor UI Fixes

--- src/Components/ProjectToggle/ProjectToggle.js
+++ src/Components/ProjectToggle/ProjectToggle.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProjectToggle.scss'
 import { NavLink } from 'react-router-dom'
+import { ProjectContext } from '../../contexts/ProjectContext';
+
 
 const ProjectToggle = () => {
+    const { selectedProject } = useContext(ProjectContext);
     return (
         <div className='project-toggle__container'>
 
-            <div className='project-toggle__title'> PROJECT LIFEBOAT</div>
+            <div className='project-toggle__title'>{selectedProject.name}</div>
             <div className="project-toggle__NavbarItems">
                                 <NavLink exact activeClassName="project-toggle__NavLinksActive" className="project-toggle__NavLinks__desc" to='/description'>Description</NavLink>
                                 <NavLink exact activeClassName="project-toggle__NavLinksActive" className="project-toggle__NavLinks" to='/requirements'>Requirements</NavLink>
