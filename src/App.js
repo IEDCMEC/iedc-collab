8febdc2aceb4c5577a79ebb71159c1711fd5bf48
jaison080 <jaisondennis080@gmail.com>
2022-09-07 10:20:47 +0530

    Updated routes and stuff

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Landing from "./Pages/Landing/Landing";
 import Collab from "./Pages/Collab/Collab";
 import initialize from "./Firebase/firebase";
 import Developers from "./Pages/Developers/Developers";
-import ProjectDetail from "./Pages/ProjectDetail/ProjectDetail";
-// import Navbar from "./Components/NavigateBar/NavigateBar";
 import { ProjectProvider } from "./contexts/ProjectContext";
 import MyProfile from "./Pages/MyProfile/MyProfile";
 import { ToastContainer } from "react-toastify";
@@ -16,8 +14,9 @@ import ScrollToTop from "./Utils/ScrollToTop";
 import Ideas from "./Pages/Ideas/Ideas";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import ProjectToggle from "./Components/ProjectToggle/ProjectToggle"
-import ProjectNav from "./Components/ProjectNav";
+import Description from "./Pages/ProjectDetail/Description";
+import Requirements from "./Pages/ProjectDetail/Requirements";
+import Discussion from "./Pages/ProjectDetail/Discussion";
 
 initialize();
 
@@ -34,13 +33,13 @@ function App() {
             <Route Route path="/collab" component={Collab} />
             <Route Route path="/projects" component={Projects} />
             <Route Route path="/developers" component={Developers} />
-            <Route Route path="/project-detail" component={ProjectDetail} />
+            <Route Route path="/description" component={Description} />
+            <Route Route path="/discussion" component={Discussion} />
+            <Route Route path="/requirements" component={Requirements} />
             {/* also add project name or id */}
             <Route Route path="/profile" component={MyProfile} />
             {/* endpoint has to be chaged with username */}
             <Route Route path="/ideas" component={Ideas} />
-            <Route Route path="/test" component={ProjectToggle} />
-            <Route Route path="/test1" component={ProjectNav} />
           </Switch>
         </BrowserRouter>
       </ProjectProvider>
--- src/Components/ProjectToggle/ProjectToggle.js
+++ src/Components/ProjectToggle/ProjectToggle.js
@@ -9,9 +9,9 @@ const ProjectToggle = () => {
             <div className='project-toggle__title'> PROJECT LIFEBOAT</div>
 
             <div className='project-toggle__options'>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/test'}>Description</NavLink>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/test'}>Requirements</NavLink>
-                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/test'}>Discussion</NavLink>
+                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/description'}>Description</NavLink>
+                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/requirements'}>Requirements</NavLink>
+                <NavLink exact activeClassName='project-toggle__NavLinksActive' classname="project-toggle__NavLinks " to={'/discussion'}>Discussion</NavLink>
 
             </div>
 
