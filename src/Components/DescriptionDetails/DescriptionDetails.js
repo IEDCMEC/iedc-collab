3d31b1b751dd11211d8f53606b11abbf1e841487
jaison080 <jaisondennis080@gmail.com>
2022-09-09 09:39:25 +0530

    Made project description page

--- src/Components/DescriptionDetails/DescriptionDetails.js
+++ src/Components/DescriptionDetails/DescriptionDetails.js
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { ProjectContext } from "../../contexts/ProjectContext";
 import "./DescriptionDetails.scss";
+import Phoneicon from "../../assets/phone.svg";
+import Mail from "../../assets/mail.svg";
+import Github from "../../assets/github.svg";
 import Tag from '../../assets/tag.svg'
 const DescriptionDetails = () => {
     const { selectedProject } = useContext(ProjectContext);
@@ -14,28 +17,69 @@ const DescriptionDetails = () => {
         </div>
       </div>
       <div className="description__other">
-        <div className="description__other-team">TEAM MEMBERS</div>
+      
         {selectedProject.teamMembers?.length && (
-                  <div className="description__other-members">
-                    <ol>
-                      {selectedProject.teamMembers.map((member) => (
-                        <li key={member}>{member}</li>
-                      ))}
-                    </ol>
-                </div>
+          <div>
+          <div className="description__other-team">TEAM MEMBERS</div>
+             <div className="description__other-members">
+             {selectedProject.teamMembers.map((member,index) => (
+               <div className="description__other-member">{index+1}. {member}</div>
+               ))}</div>
+      </div>)}
+      {selectedProject.links?.length ? (  
+        <div>       
+        <div className="description__other-reference">REFERENCES</div>
+        
+              <div
+                className="ProjectDetail-linkdiv"
+                style={{ display: "flex" }}
+              ></div></div>
+            ) : (
+              ""
             )}
-        <div className="description__other-team">REFERENCES</div>
-        <div className="description__other-references"><a href="www.tata.com" target='_blank'>www.tata.com</a></div>
-        <div className="description__other-tags"> <div className="description__other-tag"><img src={Tag} alt="tag"  /></div>
+            <div className="description__other-references">
+            {selectedProject.links?.map((link) => (
+              
+                <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  href={link.startsWith("http") ? link : "http://" + link}
+                >
+                  {link}
+                </a>
+            ))}</div>
+             
         {selectedProject.tags?.length ? (
+          <div className="description__other-tags"> <div className="description__other-tag"><img src={Tag} alt="tag"  /></div>
               <div className="description__tag">
                     {selectedProject.tags.map((tag) => (
                   <p>#{tag}</p>))}
               </div>
+              </div>
             ) : (
               ""
             )}
-      </div>
+              <div className="description-container__controls">
+            <a
+              href={`tel:${selectedProject.contactNo}`}
+            >
+              <img src={Phoneicon} alt="phone-icon"></img>
+            </a>
+            <a href={`mailto: ${selectedProject.leaderEmail}`}>
+              <img src={Mail} alt="mail"></img>
+            </a>
+            {selectedProject.githubLink.length ? (
+              <a
+                href={selectedProject.githubLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Github} alt="github"></img>
+              </a>
+            ) : (
+              ""
+            )}
+          </div>
       </div>
        
     </>
