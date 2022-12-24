3682154bba7634dcf49d5f31d33a6c5299d0bf19
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-24 16:58:25 +0530

    added all

--- src/Components/SuspenseLoader/SuspenseLoader.js
+++ src/Components/SuspenseLoader/SuspenseLoader.js
@@ -4,10 +4,10 @@ const SuspenseLoader = () => {
   return (
     <div className="loader__enwrapper">
       <div className="loader__suspense_container">
-        <span className="one"></span>
-        <span className="two"></span>
-        <span className="three"></span>
-        <span className="four"></span>
+        <span  className="one spanner"></span>
+        <span className="two spanner"></span>
+        <span className="three spanner"></span>
+        <span className="four spanner"></span>
       </div>
     </div>
   );
