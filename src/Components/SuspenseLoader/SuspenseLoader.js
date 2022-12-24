fdc0abe14d0de89887fc510cb0c8c8f3e3d4beb6
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-24 14:55:15 +0530

    all set

--- src/Components/SuspenseLoader/SuspenseLoader.js
+++ src/Components/SuspenseLoader/SuspenseLoader.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './SuspenseLoader.scss'
 const SuspenseLoader = () => {
   return (
-    <div class="container">
+    <div className="loader__enwrapper">
+         <div class="loader__suspense_container">
   <span class="one"></span>
   <span class="two"></span>
   <span class="three"></span>
   <span class="four"></span>
 </div>
+    </div>
+   
   )
 }
 
