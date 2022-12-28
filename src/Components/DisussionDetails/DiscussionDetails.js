dd347e92771b643dc7da6b990090ab80d66abfaf
jaison080 <jaisondennis080@gmail.com>
2022-12-28 10:06:44 +0530

    Fixed Coming Soon Style

--- src/Components/DisussionDetails/DiscussionDetails.js
+++ src/Components/DisussionDetails/DiscussionDetails.js
@@ -1,8 +1,18 @@
-import './DiscussionDetails.scss';
-const DiscussionDetails = ({selectedProject}) => {
-    return(
-        <><h1>Coming Soon....</h1>
-        </>
-    );
-}
-export default DiscussionDetails
+import "./DiscussionDetails.scss";
+const DiscussionDetails = ({ selectedProject }) => {
+  return (
+    <>
+      <div className="ideas__container">
+        <div className="ideas__box">
+          <div className="ideas__animation">
+            <div class="ideas__one ideas__spin-one"></div>
+            <div class="ideas__two ideas__spin-two"></div>
+            <div class="ideas__three ideas__spin-one"></div>
+          </div>
+          <h1>Coming Soon....</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+export default DiscussionDetails;
