69fe05b6748ee489d19ee6bc9daf40c9c0d1274b
jaison080 <jaisondennis080@gmail.com>
2022-12-28 11:25:31 +0530

    have added 1mb restriction for project images

--- src/Components/ProjectModal/ProjectModal.js
+++ src/Components/ProjectModal/ProjectModal.js
@@ -173,32 +173,38 @@ const NewProjectForm = ({ onClose, project }) => {
                   required
                   onBlur={props.handleBlur("photo")}
                   onChange={async (e) => {
-                    try {
-                      const results = await compress.compress(
-                        [...e.target.files],
-                        {
-                          size: 1.5,
-                          quality: 0.7,
-                          rotate: false,
-                          resize: true,
-                        }
-                      );
-                      const img1 = results[0];
-                      console.log(img1);
-                      const base64str = results[0].data;
-                      const imgExt = img1.ext;
-                      const compressedImage = Compress.convertBase64ToFile(
-                        base64str,
-                        imgExt
-                      );
-                      setProjectPhoto(compressedImage);
-                      setProjectPhotoName(e.target.files[0].name);
-                      setImage(URL.createObjectURL(compressedImage));
-                    } catch (error) {
-                      setProjectPhoto(e.target.files[0]);
-                      setProjectPhotoName(e.target.files[0].name);
-                      setImage(URL.createObjectURL(e.target.files[0]));
-                      console.log("Error in compressing: " + error);
+                    if (e.target.files[0].size > 1048576) {
+                      toast("File size should be less than 1MB", {
+                        autoClose: 2000,
+                      });
+                    } else {
+                      try {
+                        const results = await compress.compress(
+                          [...e.target.files],
+                          {
+                            size: 1.5,
+                            quality: 0.7,
+                            rotate: false,
+                            resize: true,
+                          }
+                        );
+                        const img1 = results[0];
+                        console.log(img1);
+                        const base64str = results[0].data;
+                        const imgExt = img1.ext;
+                        const compressedImage = Compress.convertBase64ToFile(
+                          base64str,
+                          imgExt
+                        );
+                        setProjectPhoto(compressedImage);
+                        setProjectPhotoName(e.target.files[0].name);
+                        setImage(URL.createObjectURL(compressedImage));
+                      } catch (error) {
+                        setProjectPhoto(e.target.files[0]);
+                        setProjectPhotoName(e.target.files[0].name);
+                        setImage(URL.createObjectURL(e.target.files[0]));
+                        console.log("Error in compressing: " + error);
+                      }
                     }
                   }}
                   type="file"
