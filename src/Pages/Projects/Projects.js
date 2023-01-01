ccc4c432dc6e0cb62b18a4b59e306ddd4b3a79c6
jaison080 <jaisondennis080@gmail.com>
2023-01-01 11:52:21 +0530

    Committing before modifying architeture

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Projects.scss";
 import { Container } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
@@ -6,9 +6,13 @@ import MainLayout from "../../Components/MainLayout/MainLayout";
 // import { getProjects } from "../../Firebase/firebase";
 import SuspenseLoader from "../../Components/SuspenseLoader/SuspenseLoader";
 import { ProjectContext } from "../../contexts/ProjectContext";
+import { Pagination } from "@mui/material";
 const Projects = () => {
   // const [projects, setProjects] = useState([]);
   const { projects, loading } = useContext(ProjectContext);
+  const [pages, setPages] = useState(0);
+  const [page, setPage] = useState(0);
+  const [works, setWorks] = useState([]);
   // const [loading, setLoading] = useState(true);
   // const getWorks = async () => {
   //   await getProjects().then(async function (snapshot) {
@@ -24,11 +28,21 @@ const Projects = () => {
   //   getWorks();
 
   // }, [projects]);
+  useEffect(() => {
+    setPage(0);
+    setPages(Math.ceil(projects.length / 12));
+    setWorks(projects.slice(page * 12, page * 12 + 12));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projects]);
+  useEffect(() => {
+    setWorks(projects.slice(page * 12, page * 12 + 12));
+  }, [page]);
   const history = useHistory();
   const handleClick = (p) => {
     history.push(`/projects/${p.id}`);
   };
-  
+
   if (loading) {
     return (
       <div>
@@ -41,43 +55,57 @@ const Projects = () => {
   return (
     <>
       <MainLayout route={"Projects"}>
-        <Container className="landing">
-          <h3 style={{ textAlign: "center" }}>
-            {projects.length === 0 ? "NOT FOUND" : "PROJECTS"}
-          </h3>
-
-          {projects.map((project) => (
-            <div
-              data-aos="zoom-in"
-              key={project.id}
-              className="cards"
-              onClick={() => handleClick(project)}
-            >
-              <div className="col-centered">
-                <div className="card" style={{ justifyContent: "center" }}>
-                  <div className="card__image-holder">
-                    <img
-                      className="img-fluid"
-                      src={
-                        project.projectPhoto ||
-                        "https://images.unsplash.com/photo-1639413665566-2f75adf7b7ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-                      }
-                      alt="project banner"
-                    />
-                  </div>
-                  <div className="card-title">
-                    <h2 className="justify-content-center text-uppercase">
-                      {project.name}
-                      <small className="justify-content-center text-capitalize">
-                        {project.leader_name}
-                      </small>
-                    </h2>
+        <div className="projects_landing">
+          <div>
+            <h3 style={{ textAlign: "center",paddingTop:"3rem" }}>
+              {projects.length === 0 ? "NOT FOUND" : "PROJECTS"}
+            </h3>
+          </div>
+          <div className="projects_cards">
+            {works.map((project) => (
+              <div
+                data-aos="zoom-in"
+                key={project.id}
+                className="cards"
+                onClick={() => handleClick(project)}
+              >
+                <div className="col-centered">
+                  <div className="card" style={{ justifyContent: "center" }}>
+                    <div className="card__image-holder">
+                      <img
+                        className="img-fluid"
+                        src={
+                          project.projectPhoto ||
+                          "https://images.unsplash.com/photo-1639413665566-2f75adf7b7ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                        }
+                        alt="project banner"
+                      />
+                    </div>
+                    <div className="card-title">
+                      <h2 className="justify-content-center text-uppercase">
+                        {project.name}
+                        <small className="justify-content-center text-capitalize">
+                          {project.leader_name}
+                        </small>
+                      </h2>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Container>
+            ))}
+          </div>
+          <Pagination
+            count={pages}
+            page={page + 1}
+            onChange={(e, val) => {
+              setPage(val - 1);
+            }}
+            color="primary"
+            sx={{
+              paddingTop: "5rem",
+            }}
+          />
+        </div>
       </MainLayout>
     </>
   );
