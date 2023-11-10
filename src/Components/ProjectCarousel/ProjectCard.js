7a0a27b42c94e1573459b25abd7cb85dbde9e2ef
Nikhil M <nikhil.mecheril@gmail.com>
2023-11-10 15:49:53 +0530

    Added MUI ToolTip

--- src/Components/ProjectCarousel/ProjectCard.js
+++ src/Components/ProjectCarousel/ProjectCard.js
@@ -1,24 +1,40 @@
 import React, { useMemo } from "react";
 import "./ProjectCarousel.scss";
-// import { ProjectContext } from "../../contexts/ProjectContext";
+import Tooltip from "@mui/material/Tooltip";
 
 function ProjectCard({ name, teamLeader, projectId, project }) {
   const isSelected = useMemo(() => projectId === project.id, [
     projectId,
     project,
   ]);
+
+  const arr = name.split(" ");
+  const firstFiveWords = arr.slice(0, 5).join(" "); // Take the first five words
+
+  const count = arr.filter((word) => word !== "").length;
+
+  const renderContent = () => (
+    <div
+      className="project-card"
+      style={{
+        backgroundColor: isSelected ? "#AC2727" : "#FFFFFF",
+        color: isSelected ? "#FFFFFF" : "#AC2727",
+      }}
+    >
+      <div className="project-card__title">{firstFiveWords}</div>
+      <div className="project-card__lead">{teamLeader}</div>
+    </div>
+  );
+
   return (
     <>
-      <div
-        className="project-card"
-        style={{
-          backgroundColor: isSelected ? "#AC2727" : "#FFFFFF",
-          color: isSelected ? "#FFFFFF" : "#AC2727",
-        }}
-      >
-        <div className="project-card__title">{name}</div>
-        <div className="project-card__lead">{teamLeader}</div>
-      </div>
+      {count > 5 ? (
+        <Tooltip style={{ backgroundColor: "black" }} title={name}>
+          {renderContent()}
+        </Tooltip>
+      ) : (
+        renderContent()
+      )}
     </>
   );
 }
