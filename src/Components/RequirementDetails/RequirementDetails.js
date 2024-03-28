40cb68c89bf0cf8c4d0bcef248627dcb8d19b805
Subru-37 <mdl21ec119@mec.ac.in>
2024-03-28 15:21:10 +0530

    ui fixes in project description

--- src/Components/RequirementDetails/RequirementDetails.js
+++ src/Components/RequirementDetails/RequirementDetails.js
@@ -84,7 +84,7 @@ const RequirementDetails = (props) => {
               {props.selectedProject?.hiring?.map((role, index) => {
                 return (
                   <div className="requirement__other-skill" key={index}>
-                    o {role}
+                     {role}
                   </div>
                 );
               })}
@@ -111,7 +111,7 @@ const RequirementDetails = (props) => {
           ) : (
             ""
           )}
-          {currentUser ? (
+          {/* {currentUser ? (
             <a href={`mailto: ${props.selectedProject.leaderEmail}`}>
               <IoMdMail
                 color="#9e0000"
@@ -121,7 +121,7 @@ const RequirementDetails = (props) => {
             </a>
           ) : (
             ""
-          )}
+          )} */}
 
           {/* {props.selectedProject.githubLink.length ? (
             <a
